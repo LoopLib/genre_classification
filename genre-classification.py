5b8d38cffbe7ef7cbcbf04b1f07c38fcae33b025
david <dvlasic000@gmail.com>
2025-02-09 13:24:30 +0000

    Enhance imports structure

--- genre-classification.py
+++ genre-classification.py
@@ -1,36 +1,34 @@
+# STANDARD PYTHON LIBRARIES
 # Provides functions for interacting with OS (paths, files, directories)
 import os
-
 # Used to manage warning messages
 import warnings
 
+# DATA PROCESSING 
 # Package for scientific computing with Python (arrays and mathematical functions)
 import numpy as np
-
 # Used for data manipulation and analysis (DataFrame structure)
 import pandas as pd
 
+# AUDIO PROCESSING 
 # Library for audio analysis, specifically for loading audio files and extracting features
 import librosa
 
+# MACHINE LEARNING 
 # For splitting data into training and testing sets
 from sklearn.model_selection import train_test_split
-
 # For encoding labels and scaling features
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-
 # For evaluating the model
 from sklearn.metrics import classification_report, confusion_matrix
-
 # Random Forest model for classification
 from sklearn.ensemble import RandomForestClassifier
-
 # Importing GridSearchCV for hyperparameter tuning
 from sklearn.model_selection import GridSearchCV
 
+# UTILITIES
 # Used to display progress bars during iterative processes
 from tqdm import tqdm
-
 # For saving (dump) and loading (load) Python objects, e.g., trained models
 from joblib import dump, load  # Import joblib for model persistence
 
